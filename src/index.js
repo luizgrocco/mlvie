0cf5fa3d54614faa83d45e295d39df758f85610b
Luiz Rocco <luizgustavorocco@hotmail.com>
2022-01-07 01:19:34 -0300

    Added eslint and prettier to the project and to CI/CD

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as Sentry from "@sentry/react"
-import { Integrations } from "@sentry/tracing"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 // process.end.NODE_ENV === 'production' && Sentry.init...
 Sentry.init({
@@ -15,16 +15,16 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
