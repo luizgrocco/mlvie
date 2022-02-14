f0ac93f8a84f2a647a865dfe27a768d2f84cf2a5
Luiz Rocco <luizgustavorocco@hotmail.com>
2022-02-14 00:51:34 -0300

    Added canvas and tensorflow to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as Sentry from "@sentry/react";
+import * as tf from "@tensorflow/tfjs";
+import CanvasDraw from "react-canvas-draw";
+
+// const canvasProps = {
+//   color: "#ffc600",
+//   width: 400,
+//   height: 400,
+//   brushRadius: 10,
+//   lazyRadius: 12,
+//   backgroundImg:
+//     "https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg",
+//   imgs: [
+//     "https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg",
+//     "https://i.imgur.com/a0CGGVC.jpg",
+//   ],
+// };
 
 function App() {
+  const canvas = useRef(null);
+
+  const handleClear = () => {
+    canvas.current.clear();
+  };
+
+  const handleSaveData = () => {
+    const ctx = canvas.current.canvas.drawing.getContext("2d");
+    const img = ctx.getImageData(0, 0, 30, 30);
+    const imgTensor = tf.browser.fromPixels(img);
+    console.log(imgTensor);
+  };
+
+  const printProps = () => {
+    console.log(canvas);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <CanvasDraw ref={canvas} hideGrid lazyRadius={0} catenaryColor="#444" />
+        <button onClick={handleClear}>Clear</button>
+        <button onClick={handleSaveData}>Save Data</button>
+        <button onClick={printProps}>Print Props</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
