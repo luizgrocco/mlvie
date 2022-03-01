e8d8ecf9c7583e38f78cfacb2e69436fab3d6dda
Luiz Rocco <luizgustavorocco@hotmail.com>
2022-03-01 04:49:49 -0300

    Removed buttons in favor of automatic behavior, added more styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as Sentry from "@sentry/react";
 import * as tf from "@tensorflow/tfjs";
 import { useDebounce } from "react-use";
 import CanvasDraw from "react-canvas-draw";
+import { CircularProgress } from "@mui/material";
 
 // const canvasProps = {
 //   color: "#ffc600",
@@ -31,54 +32,6 @@ function App() {
     canvasDraw.current.clear();
   };
 
-  const handleDraw = () => {
-    // Convert the canvas pixels to a Tensor of the model's input shape
-    let imgTensor = tf.browser.fromPixels(canvasDraw.current.canvas.drawing, 1);
-    imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [28, 28]);
-    imgTensor = imgTensor.reshape([-1, 28, 28, 1]);
-    imgTensor = tf.cast(imgTensor, "float32");
-    imgTensor = tf.div(imgTensor, tf.scalar(255));
-    setImage(imgTensor);
-  };
-
-  const loadModel = useCallback(async () => {
-    try {
-      const model = await tf.loadLayersModel(
-        // eslint-disable-next-line no-undef
-        process.env.PUBLIC_URL + "/assets/models/my-model/model.json"
-      );
-      setModel(model);
-      console.log("Model Loaded.");
-      // console.log(model);
-    } catch (e) {
-      console.log("Failed to load model! Error: ", e);
-    }
-  }, []);
-
-  const predict = useCallback(async () => {
-    await tf.tidy(() => {
-      // Make and format predictions
-      const output = model.predict(image);
-      const outputArray = Array.from(output.dataSync());
-      const prediction = Math.max(...outputArray);
-      setPredictionValue(outputArray.indexOf(prediction));
-    });
-  }, [model, image]);
-
-  useEffect(() => {
-    tf.ready().then(() => {
-      loadModel();
-    });
-  }, [loadModel]);
-
-  useDebounce(
-    () => {
-      if (model && image) predict();
-    },
-    1000,
-    [image]
-  );
-
   const handlePreviewData = () => {
     const imgTensor = tf.browser.fromPixels(
       canvasDraw.current.canvas.drawing,
@@ -87,25 +40,90 @@ function App() {
     tf.browser.toPixels(
       tf.image
         .resizeBilinear(imgTensor, [28, 28])
-        // .resizeNearestNeighbor(imgTensor, [28, 28])
         .cast("float32")
         .div(tf.scalar(255)),
-      document.getElementsByClassName("secondary-canvas")[0]
+      document.getElementsByClassName("pre-processing-canvas")[0]
     );
-    // const test2 = tf.browser.fromPixels(document.getElementById("canvas2"), 4);
-    // console.log(test2.dataSync());
   };
 
-  // const printProps = () => {
+  const handleDraw = () => {
+    // Convert the canvas pixels to a Tensor of the model's input shape
+    let imgTensor = tf.browser.fromPixels(canvasDraw.current.canvas.drawing, 1);
+    imgTensor = tf.image.resizeBilinear(imgTensor, [28, 28]);
+    imgTensor = imgTensor.reshape([-1, 28, 28, 1]);
+    imgTensor = tf.cast(imgTensor, "float32");
+    imgTensor = tf.div(imgTensor, tf.scalar(255));
+    setImage(imgTensor);
+    setIsLoading(true);
+  };
+
+  // const handlePrintProps = () => {
   //   console.log(canvasDraw);
   // };
+
+  // Hooks
+
+  // const loadModel = useCallback(async () => {
+  //   try {
+  //     const model = await tf.loadLayersModel(
+  //       // eslint-disable-next-line no-undef
+  //       process.env.PUBLIC_URL + "/assets/models/my-model/model.json"
+  //     );
+  //     setModel(model);
+  //     console.log("Model Loaded.");
+  //   } catch (e) {
+  //     console.log("Failed to load model! Error: ", e);
+  //   }
+  // }, []);
+
+  const predict = useCallback(async () => {
+    await tf.tidy(() => {
+      const output = model.predict(image);
+      const outputArray = Array.from(output.dataSync());
+      const prediction = Math.max(...outputArray);
+      setPredictionValue(outputArray.indexOf(prediction));
+    });
+    handlePreviewData();
+    handleClear();
+    setIsLoading(false);
+  }, [model, image]);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      try {
+        const model = await tf.loadLayersModel(
+          // eslint-disable-next-line no-undef
+          process.env.PUBLIC_URL + "/assets/models/my-model/model.json"
+        );
+        setModel(model);
+        console.log("Model Loaded.");
+      } catch (e) {
+        console.log("Failed to load model! Error: ", e);
+      }
+    };
+
+    tf.ready().then(() => {
+      loadModel();
+    });
+  }, []);
+
+  useDebounce(
+    () => {
+      if (model && image) {
+        setIsLoading(true);
+        predict();
+      }
+    },
+    1000,
+    [image]
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="header-container">
           <img src="assets/images/tagview_logo.webp" alt="tagview-logo" />
-          <div className="meets">meets</div>
+          <span className="meets">meets</span>
           <img
             src="assets/images/tensorflow-01.png"
             alt="tensorflow-logo+text"
@@ -113,46 +131,39 @@ function App() {
             height={150}
           />
         </div>
-        <div className="canvases-container">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <div style={{ border: "5px solid orange" }}>
-              <CanvasDraw
-                ref={canvasDraw}
-                hideGrid
-                lazyRadius={0}
-                // catenaryColor="#FFF"
-                brushColor="#FFF"
-                backgroundColor="#000"
-                onChange={handleDraw}
-              />
-            </div>
-            <button onClick={handleClear}>Clear</button>
-          </div>
-          <div className="prep-data-container">
-            <button onClick={handlePreviewData}>Pré-visualizar</button>
-            <canvas className="secondary-canvas"></canvas>
-          </div>
+      </header>
+      <div className="canvases-container">
+        <div className="drawing-canvas">
+          <CanvasDraw
+            ref={canvasDraw}
+            hideGrid
+            lazyRadius={0}
+            brushColor="#FFF"
+            backgroundColor="#000"
+            onChange={handleDraw}
+          />
         </div>
-        <div className="prediction-container">
+        <canvas className="pre-processing-canvas"></canvas>
+      </div>
+      <div className="prediction-container">
+        <div className="flex">
           <img
             src="assets/images/tensorflow_logo.png"
             alt="tensorflow-logo"
             width="50"
             height="50"
           />
-          <input
-            readOnly
-            className="prediction"
-            value={predictionValue === null ? "N/A" : predictionValue}
-          />
+          <span className="predict">Prediction:</span>
+          {isLoading && (
+            <CircularProgress className="circular-progress" color="inherit" />
+          )}
         </div>
-      </header>
+        <input
+          readOnly
+          className="prediction"
+          value={predictionValue === null ? "N/A" : predictionValue}
+        />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // process.end.NODE_ENV === 'production' && Sentry.init...
 Sentry.init({
